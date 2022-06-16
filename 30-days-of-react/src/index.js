084eddc66e8e0ef7d3333e23da90819e9a3d60a3
fuadh246 <93026516+fuadh246@users.noreply.github.com>
2022-06-16 16:37:26 -0400

    msg

--- 30-days-of-react/src/index.js
+++ 30-days-of-react/src/index.js
@@ -1,17 +1,91 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const welcome = 'Welcome to 30 Days Of React'
+const title = 'Getting Started React'
+const subtitle = 'JavaScript Library'
+const author = {
+  firstName: 'Asabeneh',
+  lastName: 'Yetayeh',
+}
+const date = 'Oct 2, 2020'
+
+
+
+const headerStyles = {
+  backgroundColor: '#61DBFB',
+  fontFamily: 'Helvetica Neue',
+  padding: 25,
+  lineHeight: 1.5,
+}
+
+const header = (
+  <header style={headerStyles}>
+    <h1>{welcome}</h1>
+    <h2>{title}</h2>
+    <h3>{subtitle}</h3>
+    <p>Author: {author.firstName} {author.lastName}</p>
+    <small>Date: {date}</small>
+  </header>
+)
+
+const numOne = 3
+const numTwo = 2
+
+const result = (
+  <p>
+    {numOne} + {numTwo} = {numOne + numTwo}
+  </p>
+)
+
+const yearBorn = 1820
+const currentYear = new Date().getFullYear()
+const age = currentYear - yearBorn
+
+const personinfo = (
+  <p>
+    {' '}
+    {author.firstName} {author.lastName} is {age} year old
+  </p>
+)
+const techs = ['HTML', 'CSS', 'JavaScript']
+const techsFormatted = techs.map((tech) => <li>{tech}</li>)
+
+
+
+const footerStyles = {
+  backgroundColor: '#61DBFB',
+}
+const footer = (
+  <footer style={footerStyles}>
+    <p>Copyright 2022</p>
+  </footer>
+)
+
+const mainStyle = {
+  backgroundColor: '#F3F0F5',
+}
+const main = (
+  <main style={mainStyle}>
+    <p>Prerequisite to get started <strong><em>react.js:</em></strong></p>
+    <ul>
+      {techsFormatted}
+    </ul>
+    {result}
+    {personinfo}
+  </main>
+)
+
+const app = (
+  <div>
+    {header}
+    {main}
+    {footer}
+  </div>
+)
+
+const rootElement = document.getElementById('root')
+
+
+ReactDOM.render(app, rootElement)
