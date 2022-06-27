0bfc4c29210caace4009db5622f587957a15eb07
fuadh246 <93026516+fuadh246@users.noreply.github.com>
2022-06-26 22:26:53 -0400

    msg

--- react_route/src/index.js
+++ react_route/src/index.js
@@ -1,96 +1,227 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom'
-import './index.css';
-import { challenges } from './challenges.js'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+} from 'react-router-dom'
 
-console.log(challenges)
-
-// {
-//   name: '30 Days Of Python',
-//     description:
-//   '30 Days of Python challenge is a step by step guide to learn Python in 30 days.',
-//     status: 'completed',
-//       days: 30,
-//         level: 'Beginners to Advanced',
-//           duration: '20 Nov 2019 - 20 Dec 2019',
-//             slug: 'pyhton',
-//               url:
-//   'https://github.com/https://https://github.com/Asabeneh/30-Days-Of-Python.com/Asabeneh/30-Days-Of-JavaScript/30-Days-Of-React',
-//     author: {
-//     firstName: 'Asabeneh',
-//       lastName: 'Yetayeh',
-//         },
-// }
 // Home component
-const MapingName = ({ name }) => challenges.map((challenge) => <div><li>{challenge.name}</li><li>{challenge.description}</li></div>)
-const MapingAbout = ({ name }) => challenges.map((challenge) => <div><li>{challenge.status}</li><li>{challenge.days}</li></div>)
-const MapingContact = ({ name }) => challenges.map((challenge) => <div><li>{challenge.level}</li><li>{challenge.duration}</li></div>)
-const MapingChallenges = ({ name }) => challenges.map((challenge) => <div><li>{challenge.slug}</li><li>{challenge.url}</li></div>)
-
-const Home = (props) => {
-  return (
-    <ol>
-      <MapingName />
-    </ol>
-  )
-}
+const Home = (props) => <h1>Welcome Home</h1>
 // About component
-const About = (props) => {
-  return (
-    <ol>
-      <MapingAbout />
-    </ol>
-  )
-}
+const About = (props) => <h1>About Us</h1>
 // Contact component
-const Contact = (props) => {
-  return (
-    <ol>
-      <MapingContact />
-    </ol>
-  )
-}
+const Contact = (props) => <h1>Contact us</h1>
+// Challenge component
+
+const challenges = [
+  {
+    name: '30 Days Of Python',
+    description:
+      '30 Days of Python challenge is a step by step guide to learn Python in 30 days.',
+    status: 'completed',
+    days: 30,
+    level: 'Beginners to Advanced',
+    duration: '20 Nov 2019 - 20 Dec 2019',
+    slug: 'pyhton',
+    url:
+      'https://github.com/https://https://github.com/Asabeneh/30-Days-Of-Python.com/Asabeneh/30-Days-Of-JavaScript/30-Days-Of-React',
+    author: {
+      firstName: 'Asabeneh',
+      lastName: 'Yetayeh',
+    },
+  },
+  {
+    name: '30 Days Of JavaScript',
+    description:
+      '30 Days of JavaScript challenge is a step by step guide to learn JavaScript in 30 days.',
+    status: 'completed',
+    days: 30,
+    level: 'Beginners to Advanced',
+    duration: '1 Jan 2020 - 30 Jan 2020',
+    slug: 'javascript',
+    url: 'https://github.com/Asabeneh/30-Days-Of-JavaScript',
+    author: {
+      firstName: 'Asabeneh',
+      lastName: 'Yetayeh',
+    },
+  },
+  {
+    name: '30 Days Of React',
+    description:
+      '30 Days of React challenge is a step by step guide to learn React in 30 days.',
+    status: 'ongoing',
+    days: 30,
+    level: 'Beginners to Advanced',
+    duration: '1 Oct 2020- 30 Oct 2020',
+    slug: 'react',
+    url: 'https://github.com/Asabeneh/30-Days-Of-React',
+    author: {
+      firstName: 'Asabeneh',
+      lastName: 'Yetayeh',
+    },
+  },
+  {
+    name: '30 HTML and CSS',
+    description:
+      '30 Days of HTML and CSS challenge is a step by step guide to learn HTML and CSS in 30 days.',
+
+    status: 'coming',
+    days: 30,
+    level: 'Beginners to Advanced',
+    duration: '',
+    slug: 'html-and-css',
+    url: '',
+    author: {
+      firstName: 'Asabeneh',
+      lastName: 'Yetayeh',
+    },
+  },
+  {
+    name: '30 ReactNative',
+    description:
+      '30 Days of ReactNative challenge is a step by step guide to learn ReactNative in 30 days.',
+    status: 'coming',
+    days: 30,
+    level: 'Beginners to Advanced',
+    duration: '',
+    slug: 'reactnative',
+    url: '',
+    author: {
+      firstName: 'Asabeneh',
+      lastName: 'Yetayeh',
+    },
+  },
+  {
+    name: '30 Data Analysis',
+    description:
+      '30 Days of Data Analysis challenge  is a step by step guide to learn about data, data visualization and data analysis in 30 days.',
+    status: 'coming',
+    days: 30,
+    level: 'Beginners to Advanced',
+    duration: '',
+    slug: 'data-analysis',
+    url: '',
+    author: {
+      firstName: 'Asabeneh',
+      lastName: 'Yetayeh',
+    },
+  },
+  {
+    name: '30 Machine Learning',
+    description:
+      '30 Days of Machine learning challenge  is a step by step guide to learn data cleaning, machine learning models and predictions in 30 days.',
+    status: 'coming',
+    days: 30,
+    level: 'Beginners to Advanced',
+    duration: '',
+    slug: 'machine-learning',
+    url: '',
+    author: {
+      firstName: 'Asabeneh',
+      lastName: 'Yetayeh',
+    },
+  },
+]
+
+const Challenge = ({
+  challenge: {
+    name,
+    description,
+    status,
+    days,
+    level,
+    duration,
+    author: { firstName, lastName },
+  },
+}) => (
+  <div>
+    <h1>{name}</h1>
+    <p>{level}</p>
+    <p>
+      Author: {firstName} {lastName}
+    </p>
+    {duration && (
+      <>
+        {' '}
+        <small>{duration}</small> <br />
+      </>
+    )}
+    <small>Number of days: {days}</small>
+
+    <p>{description}</p>
+  </div>
+)
+
 const Challenges = (props) => {
+  const path = window.location.pathname
+  const slug = path.split('/').slice(path.split('/').length - 1)[0]
+  const challenge = challenges.find((challenge) => challenge.slug === slug)
+  console.log(path)
   return (
-    <ol>
-      <MapingChallenges />
-    </ol>
+    <div>
+      <h1>31 Days Of React Challenge</h1>
+      <ul>
+        {challenges.map(({ name, slug }) => (
+          <li key={name}>
+            <NavLink to={`/challenges/${slug}`}>{name}</NavLink>
+            <h3>{`/challenges/${slug}`}</h3>
+          </li>
+        ))}
+        <li><NavLink to={`/challenges/py`}>Py</NavLink></li>
+      </ul>
+      <Routes>
+        <Route
+          exact
+          path={'/challenges'}
+          element={() => <h1>Choose any of the challenges</h1>}
+        />
+        <Route exact path='challenges/py' element={<Challenge challenge={challenges[0]} />} />
+      </Routes>
+    </div>
   )
 }
+
+// const Challenges = (props) => {
+//   return (
+//     <div>
+//       <h1>C</h1>
+//     </div>
+//   )
+
+// }
+
 const NotFound = (props) => <h1>The page your looking for not found</h1>
-
-const NavVar = (props) => {
-  return (
-    <ul>
-      <li>
-        <NavLink to='/'>Home</NavLink>
-      </li>
-      <li>
-        <NavLink to='/about'>About</NavLink>
-      </li>
-      <li>
-        <NavLink to='/contact'>Contact</NavLink>
-      </li>
-      <li>
-        <NavLink to='/challenges'>Challenges</NavLink>
-      </li>
-    </ul>
-  )
-}
-
+const Navbar = () => (
+  <ul>
+    <li>
+      <NavLink to='/'>Home</NavLink>
+    </li>
+    <li>
+      <NavLink to='/about'>About</NavLink>
+    </li>
+    <li>
+      <NavLink to='/contact'>Contact</NavLink>
+    </li>
+    <li>
+      <NavLink to='/challenges'>Challenges</NavLink>
+    </li>
+  </ul>
+)
 class App extends Component {
   render() {
     return (
       <Router>
         <div className='App'>
-          <NavVar></NavVar>
+          <Navbar />
           <Routes>
+            <Route exact path='/about' element={<About />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/challenges' element={<Challenges />} />
             <Route exact path='/' element={<Home />} />
-            <Route exact strict path='/about' element={<About />} />
-            <Route exact strict path='/contact' element={<Contact />} />
-            <Route exact strict path='/challenges' element={<Challenges />} />
-            <Route path="*" element={<NotFound />} />
+            <Route exact path='/challenges/py' element={<Challenge challenge={challenges[0]} />} />
+            <Route element={<NotFound />} />
           </Routes>
         </div>
       </Router>
